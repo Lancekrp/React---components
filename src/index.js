49a312837939bdc585c7d96791e446338f8ec252
Lance Purnell <lance.purnell@hotmail.fr>
2019-03-05 15:51:52 +0800

    created and the use of props.children to insert content in card

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommentDetail from './CommentDetail'
-
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatar={faker.image.avatar()}
-        blogPost="Nice post dude!"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 02:00AM"
-        avatar={faker.image.avatar()}
-        blogPost="Thumbs up!"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 06:00PM"
-        avatar={faker.image.avatar()}
-        blogPost="What's going on?"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          blogPost="Nice post dude!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 02:00AM"
+          avatar={faker.image.avatar()}
+          blogPost="Thumbs up!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 06:00PM"
+          avatar={faker.image.avatar()}
+          blogPost="What's going on?"
+        />
+      </ApprovalCard>
     </div>
   );
 };
