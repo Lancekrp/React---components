42579c726a756e1fabddf6be9e3d0b3af431ff72
Lance Purnell <lance.purnell@hotmail.fr>
2019-03-05 15:30:14 +0800

    use of props

--- src/index.js
+++ src/index.js
@@ -7,8 +7,24 @@ import CommentDetail from './CommentDetail'
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        avatar={faker.image.avatar()}
+        blogPost="Nice post dude!"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 02:00AM"
+        avatar={faker.image.avatar()}
+        blogPost="Thumbs up!"
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 06:00PM"
+        avatar={faker.image.avatar()}
+        blogPost="What's going on?"
+      />
     </div>
   );
 };
